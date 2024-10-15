b4c35c1a49dc8d87b1c9f62d80332dbcec975cf2
LDK3012 <duykhanglieng@gmail.com>
2024-10-15 11:43:02 +0700

    Info added

---
+++ app/src/main/java/com/example/langhexx/View/ProfileDetail.java
@@ -0,0 +1,14 @@
+package com.example.langhexx.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.langhexx.R;
+
+public class ProfileDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_detail);
+    }
+}
