b6522af0585222312be36ded8265a60b3c056ffe
Thanh Phúc <casioplus58@gmail.com>
2024-12-13 12:00:08 +0700

    navigate to leaderboard

--- app/src/main/java/com/example/langhexx/View/ArenaFragment.java
+++ app/src/main/java/com/example/langhexx/View/ArenaFragment.java
@@ -1,5 +1,6 @@
 package com.example.langhexx.View;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.langhexx.R;
 
@@ -61,6 +63,15 @@ public class ArenaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_arena, container, false);
+        View view = inflater.inflate(R.layout.fragment_arena, container, false);
+        LinearLayout lnrRanking = view.findViewById(R.id.lnrRanking) ;
+        lnrRanking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity() , LeaderboardActivity.class) ;
+                startActivity(intent);
+            }
+        });
+        return view ;
     }
 }
