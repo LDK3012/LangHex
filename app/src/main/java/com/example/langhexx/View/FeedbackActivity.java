0b6926383490d678ec57a49519ef4c40c3c85d38
LDK3012 <duykhanglieng@gmail.com>
2025-05-01 12:21:57 +0700

    fixed FeedbackActivity

--- app/src/main/java/com/example/langhexx/View/FeedbackActivity.java
+++ app/src/main/java/com/example/langhexx/View/FeedbackActivity.java
@@ -2,14 +2,26 @@ package com.example.langhexx.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.langhexx.Controller.LocaleHelper;
 import com.example.langhexx.R;
 
+import java.util.Locale;
+
 public class FeedbackActivity extends AppCompatActivity {
+
+    protected void attachBaseContext(Context newBase) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(newBase);
+        String savedLanguage = prefs.getString("user_language", Locale.getDefault().getLanguage()); // Lấy ngôn ngữ đã lưu, mặc định là ngôn ngữ hệ thống
+        super.attachBaseContext(LocaleHelper.onAttach(newBase, savedLanguage));
+    }
 
     Button btnBack;
 
