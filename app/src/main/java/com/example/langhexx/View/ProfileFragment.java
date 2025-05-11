5de3442f7019d3988a2551c1ee8fb4c21491a34f
Thanh Phúc <casioplus58@gmail.com>
2025-05-11 16:50:51 +0700

    vietnamese --> english

--- app/src/main/java/com/example/langhexx/View/ProfileFragment.java
+++ app/src/main/java/com/example/langhexx/View/ProfileFragment.java
@@ -93,7 +93,7 @@ public class ProfileFragment extends Fragment {
                 String lastUpdateVersion = prefs.getString("current_version", null);
                 String lastUpdateDate = prefs.getString("last_update_date", null);
 
-                StringBuilder sb = new StringBuilder("Phiên bản ");
+                StringBuilder sb = new StringBuilder("Version ");
                 sb.append(versionName);
 
                 if (lastUpdateVersion != null && lastUpdateDate != null) {
